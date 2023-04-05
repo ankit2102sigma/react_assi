6009893d5089343a878688a466a2806c7ef50341
Ankit Arora <ankit2102sigma>
2023-04-05 11:11:55 +0530

    push

---
+++ react_assi_1/new-project/src/components/Footer.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div className="footer">
+            <div className="footer_logo">
+                <img src="Image/logo.jpg" alt="Image" />
+            </div>
+            <div className="about">
+                <h3>ABOUT US</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis corrupti quam aliquid, maiores qui itaque quaerat suscipit sequi facilis impedit laborum? Maxime, adipisci? Blanditiis praesentium corrupti minima quidem repudiandae amet! Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi sapiente vero rem, labore vel sequi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id, similique.
+                </p>
+            </div>
+            <div className="rigth_footer">
+                <div className="navigation">
+                    <ul>
+                        <h2>Polices</h2>
+                        <li><a href="#">Refund and returns</a></li>
+                        <li><a href="#">policy shipping</a></li>
+                        <li><a href="#">Disclaimer Policy</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                    </ul>
+                </div>
+                <div className="navigation">
+                    <ul>
+                        <h2>Navigation</h2>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">site map</a></li>
+                        <li><a href="#">contact us</a></li>
+                        <li><a href="#">verify productsy</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="copyright">
+                <p>Copyright @ 2022 Sigma Infosolution</p>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ react_assi_1/new-project/src/components/Product.js
@@ -0,0 +1,92 @@
+import React from 'react';
+
+function ProductList() {
+    return (
+        <div>
+            <div className="product">
+                <a href="/hp-pavillion.html">
+                    <div className="container">
+                        <div className="items">
+                            <div className="img img1">
+                                <img src="Images/hp-pavillion.jpg" alt="" />
+                            </div>
+                            <div className="name">HP Pavilion Laptop 14-dv2016TU</div>
+                            <div className="price">70000 rs</div>
+                            <div className="info">
+                                12th Generation Intel® Core™ i5 processor
+                            </div>
+                        </div>
+                    </div>
+                </a>
+
+                <a href="/mac.html">
+                    <div className="container">
+                        <div className="items">
+                            <div className="img img1">
+                                <img src="Images/mac-screen.jpg" alt="" />
+                            </div>
+                            <div className="name">13‑inch MacBook Pro - Space Grey</div>
+                            <div className="price">90000 rs</div>
+                            <div className="info">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores,
+                                aspernatur.
+                            </div>
+                        </div>
+                    </div>
+                </a>
+
+                <a href="/hp-lap.html">
+                    <div className="container">
+                        <div className="items">
+                            <div className="img img1">
+                                <img src="Images/hp-pavillion._updatejpg.jpg" alt="" />
+                            </div>
+                            <div className="name">HP Laptop 15s-fq2717TU</div>
+                            <div className="price">40000 rs</div>
+                            <div className="info">
+                                11th Generation Intel® Core™ i3 processor
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div className="product">
+                <a href="elitebook.html">
+                    <div className="container">
+                        <div className="items">
+                            <div className="img img1">
+                                <img
+                                    src="Images/joshua-woroniecki-lzh3hPtJz9c-unsplash.jpg"
+                                    alt=""
+                                />
+                            </div>
+                            <div className="name">HP Elitebook x360 1030 G3 Laptop </div>
+                            <div className="price">50000 rs</div>
+                            <div className="info">
+                                Core i7 8th Gen/ 8GB/256 SSD/Windows 10
+                            </div>
+                        </div>
+                    </div>
+                </a>
+
+                <a href="samsung.html">
+                    <div className="container">
+                        <div className="items">
+                            <div className="img img1">
+                                <img src="Images/galaxybook.jpg" alt="" />
+                            </div>
+                            <div className="name">Samsung Galaxy Book2</div>
+                            <div className="price">64000 rs</div>
+                            <div className="info">
+                                12th Gen core i5 39.6cm (15.6") FHD.
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    );
+}
+
+export default ProductList;
